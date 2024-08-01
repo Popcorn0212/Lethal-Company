00093fa15e2a9852a00b0e7fdef8c244939eed01
Popcorn0212 <cwd8564@gmail.com>
2024-08-01 18:40:45 +0900

    ai 보강

---
+++ Assets/Enemy/Scripts/TraceArea.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TraceArea : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if(other.gameObject.tag == "Player")
+    //    { 
+    //        Enemy.en.Trace();
+    //    }
+        
+    //}
+}
