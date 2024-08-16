ac2a39326741c44ebb8a671ce6974a894306a123
Jogatun <jogatun0108@gmail.com>
2024-08-16 19:43:00 +0900

    Scan_2

--- Assets/Player/Scripts/Scan.cs
+++ Assets/Player/Scripts/Scan.cs
@@ -4,41 +4,45 @@ using UnityEngine;
 
 public class Scan : MonoBehaviour
 {
-    public List<string> objectNames;
+    public List<GameObject> scraps;
+    private List<Scrap> scrapComponents;
+    Scrap Scrap;
 
     void Start()
     {
-        objectNames = new List<string>();
+        scraps = new List<GameObject>();
+        scrapComponents = new List<Scrap>();
     }
 
 
     void Update()
     {
-            // 씬에 있는 모든 콜라이더를 검사합니다.
-            Collider[] allColliders = GameObject.FindObjectsOfType<Collider>();
-            foreach (Collider collider in allColliders)
+     // 씬에 있는 모든 콜라이더를 검사합니다.
+     Collider[] allColliders = GameObject.FindObjectsOfType<Collider>();
+        foreach (Collider collider in allColliders)
+        {
+            // 오브젝트의 월드 좌표를 카메라의 뷰포트 좌표로 변환합니다.
+            Vector3 viewportPos = Camera.main.WorldToViewportPoint(collider.transform.position);
+
+
+            // 뷰포트 좌표가 유효한지 확인합니다.
+            if (viewportPos.z > 0 && viewportPos.x >= 0 && viewportPos.x <= 1 && viewportPos.y >= 0 && viewportPos.y <= 1)
             {
-                // 오브젝트의 월드 좌표를 카메라의 뷰포트 좌표로 변환합니다.
-                Vector3 viewportPos = Camera.main.WorldToViewportPoint(collider.transform.position);
-
-
-                    // 뷰포트 좌표가 유효한지 확인합니다.
-                    if (viewportPos.z > 0 && viewportPos.x >= 0 && viewportPos.x <= 1 && viewportPos.y >= 0 && viewportPos.y <= 1)
+                if (!IsObstructed(collider))
+                {
+                    if (collider.CompareTag("Scrap"))
                     {
-                        if (!IsObstructed(collider))
+                        if (Input.GetMouseButtonDown(1))
                         {
-                            if (collider.CompareTag("Scrap"))
-                            {
-                                if (Input.GetMouseButtonDown(1))
-                                {
-                                    // 카메라의 시야에 있는 오브젝트의 이름을 리스트에 추가합니다.
-                                    objectNames.Add(collider.gameObject.name);
-                                }
-                            }
+                            // 카메라의 시야에 있는 오브젝트를 리스트에 추가합니다.
+                            scraps.Add(collider.gameObject);
+                            Scrap = scraps[0].GetComponent<Scrap>();
+                            Scrap.isScaned = true;
                         }
-
                     }
+                }
             }
+        }
 
         
     }
--- Assets/Player/Scripts/Scrap.cs
+++ Assets/Player/Scripts/Scrap.cs
@@ -10,19 +10,47 @@ public class Scrap : MonoBehaviour
 
     public int scrapValue = 100;
 
+    Transform parent;
+    Transform child;
+    Transform grandchild;
+
+    public float currentTime;
+
+    public bool isScaned;
+
     void Start()
     {
-    
+        // 부모 오브젝트의 Transform을 참조합니다.
+        parent = transform;
+
+        // 첫 번째 자식 오브젝트를 참조합니다 (인덱스 0)
+        child = parent.GetChild(0);
+
+        // 그 자식 오브젝트의 첫 번째 자식 오브젝트를 찾습니다
+        grandchild = child.GetChild(0);
+
+        grandchild.gameObject.SetActive(false);
     }
 
 
     void Update()
     {
+        currentTime += Time.deltaTime;
         objectARotation = PlayerHead.eulerAngles;
         if (isGrab)
         {
             transform.eulerAngles = new Vector3(objectARotation.x, objectARotation.y, objectARotation.z);
 
+        }
+
+        if (isScaned)
+        {
+            currentTime = 0;
+            grandchild.gameObject.SetActive(true); 
+        }
+        if (currentTime > 10)
+        {
+            grandchild.gameObject.SetActive(false);
         }
     }
 
